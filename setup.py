0afa99dd2ca8425ba5e441546381e10251a97822
Ruoyu Qian <rqian@gobonfire.com>
2019-01-21 20:38:44 -0500

    added dataset

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='dataforgood',
+    version='1.0',
+    packages=['pandas',
+              'numpy',
+              'scikit-learn',
+              'pyecharts'],
+    url='https://github.com/ruoyucad/DataForGood',
+)
